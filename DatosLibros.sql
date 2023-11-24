c7a2a0a35b665cdb0b713a16034243301a025707
Migue-Alvarez <miguelalvares2018@gmail.com>
2023-11-23 21:03:56 -0500

    Subiendo los Script de la BD

---
+++ DatosLibros.sql
@@ -0,0 +1,9 @@
+INSERT INTO LIBROS VALUES("","Mi lucha","Adolf Hitler");
+INSERT INTO LIBROS VALUES("","El extrajenro","Albert Camus");
+INSERT INTO LIBROS VALUES("","Relatos cortos","Antón Chéjov");
+INSERT INTO LIBROS VALUES("","Don Quijote de la Mancha","Miguel de Cervantes");
+INSERT INTO LIBROS VALUES("","Decamerón","Giovanni Boccaccio");
+INSERT INTO LIBROS VALUES("","Middlemarch","George Eliot");
+INSERT INTO LIBROS VALUES("","Cien años de soledad","Gabriel García Márquez");
+INSERT INTO LIBROS VALUES("","Odisea","Homero");
+INSERT INTO LIBROS VALUES("","Ilíada","Homero");
